80a90e3d244508c8ef97174947f6dca6ad883120
NikitaJape01 <72643198+NikitaJape01@users.noreply.github.com>
2020-10-10 12:34:21 +0530

    Update models.py

--- newsweb/models.py
+++ newsweb/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Contact(models.Model):
-    msg_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50)
+    msg_id = models.CharField(max_length=70, default="")
+    name = models.TextField(max_length=50)
     email = models.CharField(max_length=70, default="")
-    desc = models.CharField(max_length=500, default="")
+    desc = models.TextField(max_length=500, default="")
 
 
     def __str__(self):
